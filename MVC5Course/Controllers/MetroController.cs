0505ae36cdfce269b120a19544f824cf0285e525
vuittonatehsn <vuittonatehsn@gmail.com>
2017-05-14 16:25:32 +0800

    breadcrumb

--- MVC5Course/Controllers/MetroController.cs
+++ MVC5Course/Controllers/MetroController.cs
@@ -13,5 +13,14 @@ namespace MVC5Course.Controllers
         {
             return View();
         }
+        public ActionResult Task()
+        {
+            return View();
+        }
+
+        public ActionResult Tasks()
+        {
+            return View();
+        }
     }
 }
